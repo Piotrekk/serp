4ac9face660ed373798cebb03cb3e96b382dc840
piotrekk <piotrekk>
2018-12-18 21:31:21 +0100

    Access server outside of the project directory

--- public_html/index.php
+++ public_html/index.php
@@ -1,13 +1,24 @@
 <!DOCTYPE html>
 
+<?php
+  include 'php/utils/connection.php';
+?>
+
 <html>
   <head>
+    <title>SERP - System Elektronicznej Rejestracji Pacjenta</title>
     <link rel="stylesheet" href="css/style.css">
   </head>
 
   <body>
 
-    initial
+    <?php
+      $users_query = mysqli_query($server, "SELECT * from Users") or die ("Zle sformulowane query");
+
+      while ($row = mysqli_fetch_array($users_query)) {
+        echo implode(' ', $row);
+      }
+    ?>
 
   </body>
 </html>
---
+++ public_html/php/utils/connection.php
@@ -0,0 +1,6 @@
+<?php
+  include '../../serp_config.php';
+
+  $server = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName, $dbPort) or die("Could not connect to remote server");
+  $db = mysqli_select_db($server, "serpwsb") or die ("Could not connect to remove database");
+?>
