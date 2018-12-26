2f078cd876dc3876e1616c71263d5d37002a5b1f
piotrekk <piotrekk>
2018-12-26 17:30:12 +0100

    Check if login is in database on page refresh

--- public_html/php/utils/authentication.php
+++ public_html/php/utils/authentication.php
@@ -1,10 +1,22 @@
 <?php
-  if (empty($_COOKIE["serp_user_email"]) || empty($_COOKIE["serp_user_token"])) {
+  function resetLoginRedirectToLoginPage() {
     $isLogged = false;
     echo "<script type='text/javascript'>
             window.location = '/login.php';
           </script>";
+  }
+
+  if (empty($_COOKIE["serp_user_email"]) || empty($_COOKIE["serp_user_token"])) {
+    resetLoginRedirectToLoginPage();
   } else {
-    $isLogged = true;
+    $loginQuery = mysqli_query($server, "SELECT Users.email, Logins.token FROM Logins INNER JOIN Users ON Logins.user_id = Users.id WHERE Users.id = 1") or die ("Zle sformulowane query");
+    $loginResult = mysqli_fetch_assoc($loginQuery);
+
+    if (array_values($loginResult)[0] == $_COOKIE["serp_user_email"]
+      && array_values($loginResult)[1] == $_COOKIE["serp_user_token"]) {
+        $isLogged = true;
+      } else {
+        resetLoginRedirectToLoginPage();
+      }
   }
 ?>
