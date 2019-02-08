19f0385cb3d73e0c4da09c7459a1d3f54128ef1c
piotrekk <piotrekk>
2019-02-08 22:26:27 +0100

    Fixed reseting auth

--- public_html/php/utils/authentication.php
+++ public_html/php/utils/authentication.php
@@ -12,10 +12,6 @@
     $userQuery = mysqli_query($server, "SELECT id FROM Users WHERE email LIKE '{$_COOKIE["serp_user_email"]}'") or die ("Zle sformulowane query");
     $userResult = mysqli_fetch_assoc($userQuery);
 
-    if (empty($userResult)) {
-      resetLoginRedirectToLoginPage();
-    }
-
     $userId = array_values($userResult)[0];
 
     $loginQuery = mysqli_query($server, "SELECT Users.email, Logins.token FROM Logins INNER JOIN Users ON Logins.user_id = Users.id WHERE Users.id = {$userId}") or die ("Zle sformulowane query");
