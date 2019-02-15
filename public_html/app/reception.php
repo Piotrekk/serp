24f31c18f0406a026c402511fe30d058ef728a45
piotrekk <piotrekk>
2019-02-15 22:28:46 +0100

    Added info bar with last seen visit

--- public_html/app/reception.php
+++ public_html/app/reception.php
@@ -474,6 +474,27 @@
       }
     ?>
 
+    <?php
+      if (
+        !empty($_SESSION['lastOpenPFN']) &&
+        !empty($_SESSION['lastOpenPLN']) &&
+        !empty($_SESSION['lastOpenDate']) &&
+        !empty($_SESSION['lastOpenTime']) &&
+        !empty($_SESSION['lastOpenRoom']) &&
+        !empty($_SESSION['lastOpenDFN']) &&
+        !empty($_SESSION['lastOpenDLN'])
+      ) {
+    ?>
+      <div class="p-reception__last-visit">
+        Ostatnia przeglądana wizyta:
+        <?php
+          echo "Pacjent {$_SESSION['lastOpenPFN']} {$_SESSION['lastOpenPLN']}, {$_SESSION['lastOpenDate']} {$_SESSION['lastOpenTime']}, pokój nr {$_SESSION['lastOpenRoom']}, dr {$_SESSION['lastOpenDFN']} {$_SESSION['lastOpenDLN']}";
+        ?>
+      </div>
+    <?php
+      }
+    ?>
+
     <script>
       window.onload = function() {
         closeModal('reception.php');
